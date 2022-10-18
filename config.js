15bcb557c89a06a2724a297522cc9ad9e8b77ae0
VirgoGans <77391671+VirgoGans@users.noreply.github.com>
2022-10-18 10:52:30 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6288227856331','62895336722950','6287721317870','6288292024190']
+global.premium = ['6288227856331']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'session.data'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
